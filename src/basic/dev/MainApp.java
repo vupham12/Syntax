ba475e4d59336cd802b32babdc2ff4a48a9c4983
vupham12 <phamminhvu180101@gmail.com>
2021-04-22 16:47:57 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,38 @@
+package basic.dev;
+/**
+ * Ghi chú (comment nhiều dòng)
+ * @author Admin
+ * 1. Tên lớp trùng tên file
+ * 2. Tên lớp chữ cái đầu viết chữ hoa. Nếu từ ghép thì những chữ cái đầu viết hoa
+ * 4. Tên lớp đặt phải có ý nghĩa. Ví dụ: class Peson: lớp mô tả con người
+ * - Interface: Phải có tiền tố I.Ví dụ: Ilistener
+ * - Abstrart: Phải có tiền tố Abstrart. Ví dụ: AbstrartShape
+ */
+
+public class MainApp {
+	//Comment 1 dòng cách 1. Phím nóng Ctr + /
+		//*Comment 1 dòng cách 2*/
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		/**
+		 * Cách đặt tên biến:
+		 *  1. không chứa kí tự đặt biệt  !@#$%^&*()-+=
+		 *  2.Không chứa khoảng trắng
+		 *  3.Viết thường 
+		 *  4. Không được bắt đầu bằng số
+		 */
+		int number;
+		String nameSchool;
+
+	}
+	
+	/*
+	 * tên phương thức tuân theo quy tắc camelCase
+	 */
+	public void Show() {
+	}
+
+}
